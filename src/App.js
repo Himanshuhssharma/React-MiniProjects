860ec44c3d1c55c301f19565f8bd325ed8993845
Himanshu <himanshus@newgenit.com>
2024-05-24 10:44:59 +0530

    useState

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
+import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Counter from "./Mini/Counter/Counter.jsx"
+import Hello from './Hello.jsx';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/counter" element={Counter} />
+        <Route path="/" element={<Hello/>} />
+        <Route path="/counter" element={<Counter/>} />
       </Routes>
     </Router>
   );
--- src/Mini/Counter/Counter.jsx
+++ src/Mini/Counter/Counter.jsx
@@ -1,31 +1,25 @@
-import React from 'react'
-import { useState } from 'react'
+import React from "react";
+import { useState } from "react";
 
 const Counter = () => {
+  const [count, setCount] = useState(1);
 
-    const [count, setCount] = useState(1);
-    
-    const addFunc = () =>{
+  const addFunc = () => {
+    if (count < 20) setCount(count + 1);
+  };
 
-        if(count < 20)
-        setCount(count + 1);
-    }
-    
-    const removeFunc = () =>{
-    
-        if(count > 0)
-        setCount(count - 1);
-    }
-
+  const removeFunc = () => {
+    if (count > 0) setCount(count - 1);
+  };
 
   return (
-    <>
-    <h1>Counter</h1>
-    <button onClick={addFunc}>Add</button>
-    <h4>{count}</h4>
-    <button onClick={removeFunc}>Remove</button>
-    </>
-  )
-}
+    <div>
+      <h1>Counter</h1>
+      <button onClick={addFunc}>Add</button>
+      <h4>{count}</h4>
+      <button onClick={removeFunc}>Remove</button>
+    </div>
+  );
+};
 
-export default Counter
+export default Counter;
