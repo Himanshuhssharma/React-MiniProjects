feefddae14a85a0036b68f2cc6e63692f395c3aa
Himanshu <himanshus@newgenit.com>
2024-05-24 10:49:12 +0530

    final useState

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
 import Counter from "./Mini/Counter/Counter.jsx"
 import Hello from './Hello.jsx';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Hello/>} />
         <Route path="/counter" element={<Counter/>} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
